80250b66383084f54830fe342dfce4a2461e8f20
Arctiqa <Sinitro@yandex.ru>
2024-07-10 03:39:07 +0800

    add urls Contacts, Products

--- electronic_network/urls.py
+++ electronic_network/urls.py
@@ -1,8 +1,12 @@
 from electronic_network.apps import ElectronicNetworkConfig
 from django.urls import path
 
+from electronic_network.views.contacts import ContactsListAPIView, ContactsDestroyAPIView, ContactsDetailAPIView, \
+    ContactsUpdateAPIView, ContactsCreateAPIView
 from electronic_network.views.node import NodeCreateAPIView, NodeListAPIView, NodeUpdateAPIView, NodeDestroyAPIView, \
     NodeDetailAPIView
+from electronic_network.views.product import ProductListAPIView, ProductCreateAPIView, ProductUpdateAPIView, \
+    ProductDetailAPIView, ProductDestroyAPIView
 
 app_name = ElectronicNetworkConfig.name
 
@@ -13,5 +17,16 @@ urlpatterns = [
     path('detail/<int:pk>/', NodeDetailAPIView.as_view(), name='detail'),
     path('delete/<int:pk>/', NodeDestroyAPIView.as_view(), name='delete'),
 
+    path('contacts/', ContactsListAPIView.as_view(), name='contacts-list'),
+    path('contacts/create/', ContactsCreateAPIView.as_view(), name='contacts-create'),
+    path('contacts/update/<int:pk>/', ContactsUpdateAPIView.as_view(), name='contacts-update'),
+    path('contacts/detail/<int:pk>/', ContactsDetailAPIView.as_view(), name='contacts-detail'),
+    path('contacts/delete/<int:pk>/', ContactsDestroyAPIView.as_view(), name='contacts-delete'),
+
+    path('products/', ProductListAPIView.as_view(), name='products-list'),
+    path('products/create/', ProductCreateAPIView.as_view(), name='products-create'),
+    path('products/update/<int:pk>/', ProductUpdateAPIView.as_view(), name='products-update'),
+    path('products/detail/<int:pk>/', ProductDetailAPIView.as_view(), name='products-detail'),
+    path('products/delete/<int:pk>/', ProductDestroyAPIView.as_view(), name='products-delete'),
 
 ]
