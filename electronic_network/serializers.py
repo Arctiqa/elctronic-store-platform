c13d8aab72ab1619ce8c4fa955e0d30ec02cde40
Arctiqa <Sinitro@yandex.ru>
2024-07-10 03:38:59 +0800

    add serializers Contacts, Products

--- electronic_network/serializers.py
+++ electronic_network/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 
-from electronic_network.models import SupplierNode, Product
-
-
-class NodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SupplierNode
-        fields = ('id', 'name', 'type', 'email', 'country', 'city', 'supplier', 'house_number', 'debts')
+from electronic_network.models import SupplierNode, Product, Contacts
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('node', 'name', 'model', 'release_date')
+
+
+class ContactsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contacts
+        fields = ('node', 'email', 'country', 'city', 'street', 'house')
+
+
+class NodeSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    contacts = ContactsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = SupplierNode
+        fields = ('id', 'name', 'type', 'supplier', 'debts', 'hierarchy_level', 'products', 'contacts')
