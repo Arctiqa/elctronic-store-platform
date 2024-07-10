3dbc2d3fe5a168f4da6e6ac1837cf198ef14dece
Arctiqa <Sinitro@yandex.ru>
2024-07-10 15:36:16 +0800

    only active employees have permissions to API

--- electronic_network/views/contacts.py
+++ electronic_network/views/contacts.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from electronic_network.models import Contacts
 from electronic_network.paginators import Pagination
+from electronic_network.permissions import IsActiveUser
 from electronic_network.serializers import ContactsSerializer
 
 
@@ -11,27 +12,27 @@ class ContactsListAPIView(generics.ListAPIView):
     serializer_class = ContactsSerializer
     pagination_class = Pagination
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsActiveUser]
 
 
 class ContactsCreateAPIView(generics.CreateAPIView):
     serializer_class = ContactsSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsActiveUser]
 
 
 class ContactsUpdateAPIView(generics.UpdateAPIView):
     queryset = Contacts.objects.all()
     serializer_class = ContactsSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsActiveUser]
 
 
 class ContactsDetailAPIView(generics.RetrieveAPIView):
     queryset = Contacts.objects.all()
     serializer_class = ContactsSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsActiveUser]
 
 
 class ContactsDestroyAPIView(generics.DestroyAPIView):
     queryset = Contacts.objects.all()
     serializer_class = ContactsSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsActiveUser]
